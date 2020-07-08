dc4802256ce969efef3bb9c3aa8385213dc4ec82
Jonathan Maria Parra <jonathan_maria@ucne.edu.do>
2020-07-07 23:10:50 -0400

    Elementos de la consulta de vehiculos agregados

---
+++ RentCarManager/UI/Consultas/cVehiculos.Designer.cs
@@ -0,0 +1,187 @@
+﻿namespace RentCarManager.UI.Consultas
+{
+    partial class cVehiculos
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.DataGridViewGroupBox = new System.Windows.Forms.GroupBox();
+            this.RentasDataGridView = new System.Windows.Forms.DataGridView();
+            this.ConsultaGroupBox = new System.Windows.Forms.GroupBox();
+            this.CriterioTextBox = new System.Windows.Forms.TextBox();
+            this.HastaLabel = new System.Windows.Forms.Label();
+            this.DesdeLabel = new System.Windows.Forms.Label();
+            this.FiltroComboBox = new System.Windows.Forms.ComboBox();
+            this.FiltroLabel = new System.Windows.Forms.Label();
+            this.CriterioComboBox = new System.Windows.Forms.ComboBox();
+            this.DesdeNumericUpDown = new System.Windows.Forms.NumericUpDown();
+            this.HastaNumericUpDown = new System.Windows.Forms.NumericUpDown();
+            this.DataGridViewGroupBox.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.RentasDataGridView)).BeginInit();
+            this.ConsultaGroupBox.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.DesdeNumericUpDown)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.HastaNumericUpDown)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // DataGridViewGroupBox
+            // 
+            this.DataGridViewGroupBox.Controls.Add(this.RentasDataGridView);
+            this.DataGridViewGroupBox.Location = new System.Drawing.Point(170, 0);
+            this.DataGridViewGroupBox.Name = "DataGridViewGroupBox";
+            this.DataGridViewGroupBox.Size = new System.Drawing.Size(588, 405);
+            this.DataGridViewGroupBox.TabIndex = 4;
+            this.DataGridViewGroupBox.TabStop = false;
+            // 
+            // RentasDataGridView
+            // 
+            this.RentasDataGridView.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.RentasDataGridView.Location = new System.Drawing.Point(5, 10);
+            this.RentasDataGridView.Name = "RentasDataGridView";
+            this.RentasDataGridView.Size = new System.Drawing.Size(577, 388);
+            this.RentasDataGridView.TabIndex = 0;
+            // 
+            // ConsultaGroupBox
+            // 
+            this.ConsultaGroupBox.Controls.Add(this.HastaNumericUpDown);
+            this.ConsultaGroupBox.Controls.Add(this.DesdeNumericUpDown);
+            this.ConsultaGroupBox.Controls.Add(this.CriterioComboBox);
+            this.ConsultaGroupBox.Controls.Add(this.CriterioTextBox);
+            this.ConsultaGroupBox.Controls.Add(this.HastaLabel);
+            this.ConsultaGroupBox.Controls.Add(this.DesdeLabel);
+            this.ConsultaGroupBox.Controls.Add(this.FiltroComboBox);
+            this.ConsultaGroupBox.Controls.Add(this.FiltroLabel);
+            this.ConsultaGroupBox.Location = new System.Drawing.Point(5, 0);
+            this.ConsultaGroupBox.Name = "ConsultaGroupBox";
+            this.ConsultaGroupBox.Size = new System.Drawing.Size(162, 279);
+            this.ConsultaGroupBox.TabIndex = 3;
+            this.ConsultaGroupBox.TabStop = false;
+            // 
+            // CriterioTextBox
+            // 
+            this.CriterioTextBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.CriterioTextBox.Location = new System.Drawing.Point(36, 95);
+            this.CriterioTextBox.Name = "CriterioTextBox";
+            this.CriterioTextBox.Size = new System.Drawing.Size(100, 22);
+            this.CriterioTextBox.TabIndex = 32;
+            // 
+            // HastaLabel
+            // 
+            this.HastaLabel.AutoSize = true;
+            this.HastaLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.HastaLabel.Location = new System.Drawing.Point(7, 166);
+            this.HastaLabel.Name = "HastaLabel";
+            this.HastaLabel.Size = new System.Drawing.Size(63, 16);
+            this.HastaLabel.TabIndex = 30;
+            this.HastaLabel.Text = "Nombre";
+            // 
+            // DesdeLabel
+            // 
+            this.DesdeLabel.AutoSize = true;
+            this.DesdeLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.DesdeLabel.Location = new System.Drawing.Point(6, 76);
+            this.DesdeLabel.Name = "DesdeLabel";
+            this.DesdeLabel.Size = new System.Drawing.Size(63, 16);
+            this.DesdeLabel.TabIndex = 29;
+            this.DesdeLabel.Text = "Nombre";
+            // 
+            // FiltroComboBox
+            // 
+            this.FiltroComboBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.FiltroComboBox.FormattingEnabled = true;
+            this.FiltroComboBox.Location = new System.Drawing.Point(6, 29);
+            this.FiltroComboBox.Name = "FiltroComboBox";
+            this.FiltroComboBox.Size = new System.Drawing.Size(121, 24);
+            this.FiltroComboBox.TabIndex = 27;
+            // 
+            // FiltroLabel
+            // 
+            this.FiltroLabel.AutoSize = true;
+            this.FiltroLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.FiltroLabel.Location = new System.Drawing.Point(5, 10);
+            this.FiltroLabel.Name = "FiltroLabel";
+            this.FiltroLabel.Size = new System.Drawing.Size(63, 16);
+            this.FiltroLabel.TabIndex = 26;
+            this.FiltroLabel.Text = "Nombre";
+            // 
+            // CriterioComboBox
+            // 
+            this.CriterioComboBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.CriterioComboBox.FormattingEnabled = true;
+            this.CriterioComboBox.Location = new System.Drawing.Point(43, 121);
+            this.CriterioComboBox.Name = "CriterioComboBox";
+            this.CriterioComboBox.Size = new System.Drawing.Size(121, 24);
+            this.CriterioComboBox.TabIndex = 33;
+            // 
+            // DesdeNumericUpDown
+            // 
+            this.DesdeNumericUpDown.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.DesdeNumericUpDown.Location = new System.Drawing.Point(42, 141);
+            this.DesdeNumericUpDown.Name = "DesdeNumericUpDown";
+            this.DesdeNumericUpDown.Size = new System.Drawing.Size(120, 22);
+            this.DesdeNumericUpDown.TabIndex = 34;
+            // 
+            // HastaNumericUpDown
+            // 
+            this.HastaNumericUpDown.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.HastaNumericUpDown.Location = new System.Drawing.Point(27, 198);
+            this.HastaNumericUpDown.Name = "HastaNumericUpDown";
+            this.HastaNumericUpDown.Size = new System.Drawing.Size(120, 22);
+            this.HastaNumericUpDown.TabIndex = 35;
+            // 
+            // cVehiculos
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.DataGridViewGroupBox);
+            this.Controls.Add(this.ConsultaGroupBox);
+            this.Name = "cVehiculos";
+            this.Text = "cVehiculos";
+            this.DataGridViewGroupBox.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.RentasDataGridView)).EndInit();
+            this.ConsultaGroupBox.ResumeLayout(false);
+            this.ConsultaGroupBox.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.DesdeNumericUpDown)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.HastaNumericUpDown)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox DataGridViewGroupBox;
+        private System.Windows.Forms.DataGridView RentasDataGridView;
+        private System.Windows.Forms.GroupBox ConsultaGroupBox;
+        private System.Windows.Forms.TextBox CriterioTextBox;
+        private System.Windows.Forms.Label HastaLabel;
+        private System.Windows.Forms.Label DesdeLabel;
+        private System.Windows.Forms.ComboBox FiltroComboBox;
+        private System.Windows.Forms.Label FiltroLabel;
+        private System.Windows.Forms.NumericUpDown HastaNumericUpDown;
+        private System.Windows.Forms.NumericUpDown DesdeNumericUpDown;
+        private System.Windows.Forms.ComboBox CriterioComboBox;
+    }
+}
