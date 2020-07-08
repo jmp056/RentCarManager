e9c7a237512748abbe29cc603176b815f0fb3950
Jonathan Maria Parra <jonathan_maria@ucne.edu.do>
2020-07-07 21:24:56 -0400

    Entidad rentas editada

--- RentCarManager/Entidades/Rentas.cs
+++ RentCarManager/Entidades/Rentas.cs
@@ -7,6 +7,10 @@ namespace RentCarManager.Entidades
     {
         [Key]
         public int RentaId { get; set; }
+        public string NombreCliente { get; set; }
+        public string Cedula { get; set; }
+        public string Celular { get; set; }
+        public string Direccion { get; set; }
         public DateTime FechaRegistro { get; set; }
         public int Dias { get; set; }
         public DateTime FechaInicio { get; set; }
@@ -16,6 +20,10 @@ namespace RentCarManager.Entidades
         public Rentas()
         {
             RentaId = 0;
+            NombreCliente = string.Empty;
+            Cedula = string.Empty;
+            Celular = string.Empty;
+            Direccion = string.Empty;
             FechaRegistro = DateTime.Now;
             Dias = 0;
             FechaInicio = DateTime.Now;
